8e218888a924494f1a8d7de4b7a4792b7a12f53a
yoshi <yoshitakaOkada0214@gmail.com>
2023-06-16 21:37:07 +0900

    markedをアンインストールして、ちょっとシンプルに

--- src/daily_report/components/detail/Content.tsx
+++ src/daily_report/components/detail/Content.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 
 import { DetailContentEntity } from "../../domain/DetailContentEntity";
-import { marked } from "marked";
 
 export const Content = (entity: DetailContentEntity) => {
   return (
     <div>
       <h1>{ entity.date }</h1>
       <h2>大学のこと</h2>
-      <div dangerouslySetInnerHTML={{ __html: `${marked(`${ entity.univ }`)}` }} className="detail-content"></div>
+      <div>{ entity.univ }</div>
       <h2>勉強</h2>
-      <div dangerouslySetInnerHTML={{ __html: `${marked(`${ entity.study }`)}` }} className="detail-content"></div>
+      <div>{ entity.study }</div>
       <h2>その他</h2>
-      <div dangerouslySetInnerHTML={{ __html: `${marked(`${ entity.other }`)}` }} className="detail-content"></div>
+      <div>{ entity.other }</div>
       <h2>初めましてだったこと</h2>
-      <div dangerouslySetInnerHTML={{ __html: `${marked(`${ entity.first_meet }`)}` }} className="detail-content"></div>
+      <div>{ entity.first_meet }</div>
       <h2>やりたいこと</h2>
-      <div dangerouslySetInnerHTML={{ __html: `${marked(`${ entity.wanna_do }`)}` }} className="detail-content"></div>
+      <div>{ entity.wanna_do }</div>
       <h2>1日のまとめ</h2>
-      <div dangerouslySetInnerHTML={{ __html: `${marked(`${ entity.summary }`)}` }} className="detail-content"></div>
+      <div>{ entity.summary }</div>
     </div>
   );
 };
